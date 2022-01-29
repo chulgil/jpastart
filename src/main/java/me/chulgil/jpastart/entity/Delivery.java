2d63157f8864e1fd95e7edf944cab3cd4060f4be
cglee <contact@chulgil.me>
2022-01-29 23:47:44 +0900

    Feat: 값타입 매핑

--- src/main/java/me/chulgil/jpastart/entity/Delivery.java
+++ src/main/java/me/chulgil/jpastart/entity/Delivery.java
@@ -9,13 +9,42 @@ public class Delivery extends BaseEntity {
     @Column(name="DELIVERY_ID")
     private Long Id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
+    public Long getId() {
+        return Id;
+    }
 
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
--- src/main/java/me/chulgil/jpastart/entity/Member.java
+++ src/main/java/me/chulgil/jpastart/entity/Member.java
@@ -16,33 +16,8 @@ public class Member extends BaseEntity {
     private Long id;
     private String name;
 
-    private String city;
-    private String street;
-    private String zipcode;
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+    @Embedded
+    private Address address;
 
     public Long getId() {
         return id;
@@ -60,6 +35,13 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
 
 
